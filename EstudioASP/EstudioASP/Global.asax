276dd1bb770c53d94551d9ac164daea5fc4b78c7
Santiago Cano Restrepo <santiagocano6@hotmail.com>
2016-01-21 23:20:40 -0500

    Base

---
+++ EstudioASP/EstudioASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EstudioASP.MvcApplication" Language="C#" %>
---
+++ EstudioASP/EstudioASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstudioASP.Startup))]
+namespace EstudioASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
