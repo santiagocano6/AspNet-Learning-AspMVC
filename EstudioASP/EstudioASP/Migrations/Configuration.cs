3413081f5d2995492e139b7d574113f63c718f30
Santiago Cano <santiagocano6@hotmail.com>
2016-02-19 12:23:12 -0500

    Se agregan valores por defecto a la BD

--- EstudioASP/EstudioASP/Migrations/Configuration.cs
+++ EstudioASP/EstudioASP/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace EstudioASP.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<EstudioASP.Models.ApplicationDbContext>
     {
@@ -19,13 +20,29 @@ namespace EstudioASP.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.IdiomaModels.AddOrUpdate(
+              p => p.IdiomaID,
+              new IdiomaModels { IdiomaID = 1, Nombre = "Español" },
+              new IdiomaModels { IdiomaID = 2, Nombre = "English" }
+            );
+
+            context.PaisModels.AddOrUpdate(
+              p => p.PaisID,
+              new PaisModels { PaisID = 1, Nombre = "Colombia" },
+              new PaisModels { PaisID = 2, Nombre = "España" },
+              new PaisModels { PaisID = 3, Nombre = "Perú" },
+              new PaisModels { PaisID = 4, Nombre = "Estados Unidos" },
+              new PaisModels { PaisID = 5, Nombre = "Inglaterra" }
+            );
+
+            context.UniversidadModels.AddOrUpdate(
+              p => p.UniversidadID,
+              new UniversidadModels { UniversidadID = 1, Nombre = "TdeA", PaisID = 1 },
+              new UniversidadModels { UniversidadID = 2, Nombre = "Politecnico JIC", PaisID = 1 },
+              new UniversidadModels { UniversidadID = 3, Nombre = "UdeA", PaisID = 1 },
+              new UniversidadModels { UniversidadID = 4, Nombre = "Universida de Cataluña", PaisID = 2 },
+              new UniversidadModels { UniversidadID = 5, Nombre = "Cambrish", PaisID = 4 }
+            );
         }
     }
 }
